b66bd0f1969452fbbaba61e0c7a62b60e9c43887
okayplanet <tyrick@gmail.com>
2019-02-14 18:13:27 -0700

    express correction

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/*', express.static(path.join(__dirname, 'dist')));
 
 app.get('/whitepaper', (req, res) =>{
     res.setHeader("Content-Type","application/pdf");
     res.setHeader("Content-Dispositon", "attachment; filename=" + "patreos_whitepaper_1_30_19.pdf");
     res.sendFile(path.join(__dirname + '/public/whitepaper/patreos_whitepaper_1_30_19.pdf'));
 });
+
+app.use('/*', express.static(path.join(__dirname, 'dist')));
 
 app.listen(3000);
 console.log('Listening on port 3000');
